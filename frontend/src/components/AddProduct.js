5af42a894cff575aaf8e34629e0c26c3fc6ec55e
Ringgo <112040862+Enighander@users.noreply.github.com>
2023-06-25 13:32:11 +0700

    Update AddProduct.js
    change IP to localhost

--- frontend/src/components/AddProduct.js
+++ frontend/src/components/AddProduct.js
@@ -11,7 +11,7 @@ const navigate = useNavigate();
 const saveProduct = async (e) => {
     e.preventDefault();
     try {
-        await axios.post("http://127.0.0.1:5000/products", {
+        await axios.post("http://localhost:5000/products", {
             name,
             price,
             stock
